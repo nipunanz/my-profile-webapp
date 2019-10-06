a490b3ecf0935884ac4a49c665ed9087e6111898
Nipuna Fernando <nipuna.nzone@gmail.com>
2019-10-06 15:56:03 +1100

    Add Initial Files
    - Add public folder
    - Add src folder
    - Add App module
    - Add package.json
    - Add package-lock.json
    - Add tsconfig.json
    - Add yarn.lock

---
+++ src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+
+
+
+const App: React.FC = () => {
+	
+		return (
+			<div>
+				<h1>Welcome to My Profile</h1>
+			</div>
+		);
+}
+
+export default App;
